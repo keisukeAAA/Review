0782b2b1416659532ed8424c3f90f786c68ca1de
keisukeAAA <80804434+keisukeAAA@users.noreply.github.com>
2021-06-12 07:03:46 +0900

    'Review'

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import covidReducer from "../features/covid/covidSlicse";
+
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    covid: covidReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
